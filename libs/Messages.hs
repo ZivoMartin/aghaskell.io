2e04fa2c94a56a5b92839ca448b4e014c4d1bc54
martin <martin.zivojinovic@etu.u-bordeaux.fr>
2025-03-15 14:39:01 +0100

    Refresh and killed messages

--- libs/Messages.hs
+++ libs/Messages.hs
@@ -2,10 +2,18 @@ module Messages where
 
 import Data.ByteString
 
+
+-- | x, y, size
+type PlayerData = (Int, Int, Int)
+
 -- | Messages sent by the server
 data ServerMessage =
-  -- | A simple Dummy message
-  ServerDummy
+  -- | An array representing the position and the size of all the player in the game
+  -- | The position of the player that receiv the message, eventually with a new size
+  -- | Note that the players list only contains other players
+  Refresh [PlayerData] PlayerData
+  -- | Sent when someone eat the player. 
+  | Killed
   deriving (Show, Read)
 
 -- | Messages sent by the client
